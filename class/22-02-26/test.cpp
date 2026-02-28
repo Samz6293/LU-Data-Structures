6dd6cc313c73dfc45fdd9433f9076b3baa393b27
Samz6293 <237053497+Samz6293@users.noreply.github.com>
2026-02-28 14:16:05 +0600

    remove zone identifier

--- class/22-02-26/test.cpp
+++ class/22-02-26/test.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
 using namespace std;
 
-int main () {
-    return 0;
+int main() {
+	// your code goes here
+    string S;
+    cin >> S;
+    
+    if (S[2] == 'n' and S[3] == 't' and S[4] == 'a') {
+        cout << "YES" << endl;
+    }
+    
+    else {
+        cout << "NO" << endl;
+    } 
+   
 }
